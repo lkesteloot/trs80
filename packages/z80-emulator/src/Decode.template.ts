75f560c3b6131e4b334e5ad3a36bf8a8ff0dd0aa
Lawrence Kesteloot <lk@teamten.com>
2019-12-23 17:37:09 -0800

    Handle some IX/IY opcodes.

--- packages/z80-emulator/src/Decode.template.ts
+++ packages/z80-emulator/src/Decode.template.ts
@@ -1,19 +1,103 @@
 import {Z80} from "./Z80";
-import {toHex, inc8, inc16, word, hi, lo, Flag} from "z80-test/dist";
+import {toHex, inc8, inc16, word, hi, lo, Flag, signedByte} from "z80-test/dist";
 
 const halfCarryAddTable = [0, Flag.H, Flag.H, Flag.H, 0, 0, 0, Flag.H];
 const halfCarrySubTable = [0, 0, Flag.H, 0, Flag.H, 0, Flag.H, Flag.H];
 const overflowAddTable = [0, 0, 0, Flag.V, Flag.V, 0, 0, 0];
 const overflowSubTable = [0, Flag.V, 0, 0, 0, 0, Flag.V, 0];
 
-export function decode(z80: Z80): void {
+function fetchInstruction(z80: Z80): number {
     z80.tStateCount += 4;
     const inst = z80.readByteInternal(z80.regs.pc);
     z80.regs.pc = (z80.regs.pc + 1) & 0xFFFF;
     z80.regs.r = (z80.regs.r + 1) & 0xFF;
 
+    return inst;
+}
+
+function decodeCB(z80: Z80): void {
+    const inst = fetchInstruction(z80);
+
     switch (inst) {
-        // DECODE
+        // DECODE_CB
+
+        default:
+            console.log("Unhandled opcode " + toHex(inst, 2));
+            break;
+
+    }
+}
+
+function decodeDD(z80: Z80): void {
+    const inst = fetchInstruction(z80);
+
+    switch (inst) {
+        // DECODE_DD
+
+        default:
+            console.log("Unhandled opcode " + toHex(inst, 2));
+            break;
+
+    }
+}
+
+function decodeDDCB(z80: Z80): void {
+    const inst = fetchInstruction(z80);
+
+    switch (inst) {
+        // DECODE_DDCB
+
+        default:
+            console.log("Unhandled opcode " + toHex(inst, 2));
+            break;
+
+    }
+}
+
+function decodeED(z80: Z80): void {
+    const inst = fetchInstruction(z80);
+
+    switch (inst) {
+        // DECODE_ED
+
+        default:
+            console.log("Unhandled opcode " + toHex(inst, 2));
+            break;
+
+    }
+}
+
+function decodeFD(z80: Z80): void {
+    const inst = fetchInstruction(z80);
+
+    switch (inst) {
+        // DECODE_FD
+
+        default:
+            console.log("Unhandled opcode " + toHex(inst, 2));
+            break;
+
+    }
+}
+
+function decodeFDCB(z80: Z80): void {
+    const inst = fetchInstruction(z80);
+
+    switch (inst) {
+        // DECODE_FDCB
+
+        default:
+            console.log("Unhandled opcode " + toHex(inst, 2));
+            break;
+
+    }
+}
+
+export function decode(z80: Z80): void {
+    const inst = fetchInstruction(z80);
+
+    switch (inst) {
+        // DECODE_BASE
 
         default:
             console.log("Unhandled opcode " + toHex(inst, 2));
