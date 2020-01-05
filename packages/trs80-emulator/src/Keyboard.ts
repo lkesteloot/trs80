e47f8bea3c70b296198e4d5738383ab0b235a812
Lawrence Kesteloot <lk@teamten.com>
2020-01-04 17:46:45 -0800

    Use better keyboard input mechanism.

--- packages/trs80-emulator/src/Keyboard.ts
+++ packages/trs80-emulator/src/Keyboard.ts
@@ -134,12 +134,12 @@ keyMap.set(">", new KeyInfo(5, 6, ShiftState.FORCE_DOWN));
 keyMap.set("?", new KeyInfo(5, 7, ShiftState.FORCE_DOWN));
 
 keyMap.set("Enter", new KeyInfo(6, 0, ShiftState.NEUTRAL));
-keyMap.set("Clear", new KeyInfo(6, 1, ShiftState.NEUTRAL));
-keyMap.set("Break", new KeyInfo(6, 2, ShiftState.NEUTRAL));
-keyMap.set("Up", new KeyInfo(6, 3, ShiftState.NEUTRAL));
-keyMap.set("Down", new KeyInfo(6, 4, ShiftState.NEUTRAL));
-keyMap.set("Left", new KeyInfo(6, 5, ShiftState.NEUTRAL));
-keyMap.set("Right", new KeyInfo(6, 6, ShiftState.NEUTRAL));
+keyMap.set("Tab", new KeyInfo(6, 1, ShiftState.NEUTRAL)); // Clear
+keyMap.set("Escape", new KeyInfo(6, 2, ShiftState.NEUTRAL)); // Break
+keyMap.set("ArrowUp", new KeyInfo(6, 3, ShiftState.NEUTRAL));
+keyMap.set("ArrowDown", new KeyInfo(6, 4, ShiftState.NEUTRAL));
+keyMap.set("ArrowLeft", new KeyInfo(6, 5, ShiftState.NEUTRAL));
+keyMap.set("ArrowRight", new KeyInfo(6, 6, ShiftState.NEUTRAL));
 keyMap.set(" ", new KeyInfo(6, 7, ShiftState.NEUTRAL));
 keyMap.set("Shift", new KeyInfo(7, 0, ShiftState.NEUTRAL));
 
@@ -223,112 +223,9 @@ export class Keyboard {
 
     // Convert keys on the keyboard to ASCII letters or special strings like "Enter".
     public configureKeyboard(): void {
-        // Converts a key up/down event to an ASCII character or string representing
-        // key, like "Left".
-        const eventToKey = (event: KeyboardEvent) => {
-            let key;
-            let which = event.which;
-            const shifted = event.shiftKey;
-
-            // http://www.trs-80.com/trs80-zaps-internals.htm#keyboard13
-            if (which === 13) {
-                // Enter.
-                key = "Enter";
-            } else if (which === 32) {
-                // Space.
-                key = " ";
-            } else if (which >= 65 && which < 65+26) {
-                // Letters.
-                if (!shifted) {
-                    // Make lower case.
-                    which += 32;
-                }
-                key = String.fromCharCode(which);
-            } else if (which === 48) {
-                key = shifted ? ")" : "0";
-            } else if (which === 49) {
-                key = shifted ? "!" : "1";
-            } else if (which === 50) {
-                key = shifted ? "@" : "2";
-            } else if (which === 51) {
-                key = shifted ? "#" : "3";
-            } else if (which === 52) {
-                key = shifted ? "$" : "4";
-            } else if (which === 53) {
-                key = shifted ? "%" : "5";
-            } else if (which === 54) {
-                key = shifted ? "^" : "6";
-            } else if (which === 55) {
-                key = shifted ? "&" : "7";
-            } else if (which === 56) {
-                key = shifted ? "*" : "8";
-            } else if (which === 57) {
-                key = shifted ? "(" : "9";
-            } else if (which === 8) {
-                // Backspace.
-                key = "Left"; // Left.
-
-                // Don't go back to previous page.
-                event.preventDefault();
-            } else if (which === 187) {
-                // Equal.
-                key = shifted ? "+" : "=";
-            } else if (which === 188) {
-                // Comma.
-                key = shifted ? "<" : ",";
-            } else if (which === 190) {
-                // Period.
-                key = shifted ? ">" : ".";
-            } else if (which === 16) {
-                // Shift.
-                key = "Shift";
-            } else if (which === 192) {
-                // Backtick.
-                key = shifted ? "~" : "`";
-            } else if (which === 186) {
-                // Semicolon.
-                key = shifted ? ":" : ";";
-            } else if (which === 222) {
-                // Quote..
-                key = shifted ? "\"" : "'";
-            } else if (which === 189) {
-                // Hyphen.
-                key = shifted ? "_" : "-";
-            } else if (which === 191) {
-                // Slash.
-                key = shifted ? "?" : "/";
-            } else if (which === 37) {
-                // Left arrow.
-                key = "Left";
-            } else if (which === 39) {
-                // Right arrow.
-                key = "Right";
-            } else if (which === 40) {
-                // Down arrow.
-                key = "Down";
-            } else if (which === 38) {
-                // Up arrow.
-                key = "Up";
-            } else if (which === 27) {
-                // Escape.
-                key = "Break";
-            } else if (which === 9) {
-                // Tab.
-                key = "Clear";
-
-                // Don't move focus to next field.
-                event.preventDefault();
-            } else {
-                // Ignore.
-                /// console.log(which);
-                key = "";
-            }
-
-            return key;
-        };
-
         // Handle a key event by mapping it and sending it to the emulator.
         const keyEvent = (event: KeyboardEvent, isPressed: boolean) => {
+            // Don't do anything if we're not active.
             if (!this.interceptKeys) {
                 return;
             }
@@ -343,7 +240,7 @@ export class Keyboard {
                 return;
             }
 
-            const key = eventToKey(event);
+            let key = event.key;
             if (key !== "") {
                 this.keyEvent(key, isPressed);
                 event.preventDefault();
