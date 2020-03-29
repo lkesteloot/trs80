89411c5bd8462f12f97aa2c883a1e6530a07c368
Lawrence Kesteloot <lk@teamten.com>
2020-03-28 17:13:58 -0700

    Can assemble instructions without expressions.

--- packages/z80-asm/src/Main.ts
+++ packages/z80-asm/src/Main.ts
@@ -1,15 +1,20 @@
 
 import * as fs from "fs";
 import chalk from "chalk";
+import mnemonicData from "./Opcodes.json";
+import {toHex} from "z80-base";
 
 const srcPathname = "sio_basic.asm";
 
 class Parser {
-    private line: string;
+    private readonly line: string;
     // Parsing index into the line.
     private i: number = 0;
     // Pointer to the token we just parsed.
     private previousToken: number | undefined;
+    // Decoded opcodes and parameters:
+    public binary: number[] = [];
+    public error: string | undefined;
 
     constructor(line: string) {
         this.line = line;
@@ -26,16 +31,63 @@ class Parser {
         }
 
         if (opOrlabel !== undefined) {
-            console.log("Found directive or op: " + opOrlabel);
-        }
+            // TODO: I don't know why I need that any:
+            const mnemonicInfo = (mnemonicData as any).mnemonics[opOrlabel];
+            if (mnemonicInfo !== undefined) {
+                const argStart = this.i;
+                let match = false;
 
-        if (this.isChar(';')) {
-            // Skip rest of line.
-            this.i = this.line.length;
-        }
+                for (const variant of mnemonicInfo.variants) {
+                    match = true;
 
-        if (this.i !== this.line.length) {
-            console.log(chalk.red("syntax error"));
+                    for (const token of variant.tokens) {
+                        if (token === "," || token === "(" || token === ")" || token === "+") {
+                            if (!this.foundChar(token)) {
+                                match = false;
+                            }
+                        } else if (token === "nn" || token === "nnnn" || token === "dd") {
+                            // Parse.
+                            match = false;
+                        } else {
+                            // Register or flag.
+                            const identifier = this.readIdentifier();
+                            if (identifier !== token) {
+                                match = false;
+                            }
+                        }
+
+                        if (!match) {
+                            break;
+                        }
+                    }
+
+                    if (match) {
+                        // See if it's the end of the line.
+                        if (this.isChar(';')) {
+                            // Skip rest of line.
+                            this.i = this.line.length;
+                        }
+
+                        if (this.i != this.line.length) {
+                            match = false;
+                        }
+                    }
+
+                    if (match) {
+                        this.binary = variant.opcode;
+                        break;
+                    } else {
+                        // Reset reader.
+                        this.i = argStart;
+                    }
+                }
+
+                if (!match) {
+                    this.error = "no variant found for " + opOrlabel;
+                }
+            } else {
+                this.error = "unknown mnemonic: " + opOrlabel;
+            }
         }
     }
 
@@ -81,8 +133,20 @@ class Parser {
     }
 }
 
+let address = 0;
 fs.readFileSync(srcPathname, "utf-8").split(/\r?\n/).forEach((line: string) => {
-    console.log(chalk.gray(line));
+    console.log("                 " + chalk.gray(line));
     const parser = new Parser(line);
     parser.assemble();
+    if (parser.error !== undefined) {
+        console.log("                 " + chalk.red(parser.error));
+    } else if (parser.binary.length !== 0) {
+        let result = toHex(address, 4) + " ";
+        for (const byte of parser.binary) {
+            result += " " + toHex(byte, 2);
+        }
+        result = result.padEnd(17, " ") + line;
+        console.log(result);
+        address += parser.binary.length;
+    }
 });
