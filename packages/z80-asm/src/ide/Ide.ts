3e746e9d7c5a8690fe4f0904e40c476438436794
Lawrence Kesteloot <lk@teamten.com>
2020-04-03 22:21:56 -0700

    Reassemble on change to editor text.

--- packages/z80-asm/src/ide/Ide.ts
+++ packages/z80-asm/src/ide/Ide.ts
@@ -5,6 +5,42 @@ import "codemirror/theme/mbo.css";
 import sample from "./Sample";
 import {Parser} from "../assembler/Parser";
 
+function assembleAll(cm: CodeMirror.Editor) {
+    const constants = {};
+
+    const before = Date.now();
+
+    // First pass.
+    let address = 0;
+    for (let i = 0; i < cm.lineCount(); i++) {
+        const line = cm.getLine(i);
+        const parser = new Parser(line, address, constants, true);
+        const results = parser.assemble();
+        address = results.nextAddress;
+    }
+
+    // Second pass.
+    address = 0;
+    for (let i = 0; i < cm.lineCount(); i++) {
+        const line = cm.getLine(i);
+        const parser = new Parser(line, address, constants, false);
+        const results = parser.assemble();
+
+        const addressString = results.binary.length > 0 ? results.address.toString(16).toUpperCase().padStart(4, "0") : "";
+        const addressElement = document.createTextNode(addressString) as any as HTMLElement;
+        cm.setGutterMarker(i, "gutter-address", addressElement);
+
+        const opcodeString = results.binary.map(opcode => opcode.toString(16).toUpperCase().padStart(2, "0")).join(" ");
+        const opcodeElement = document.createTextNode(opcodeString) as any as HTMLElement;
+        cm.setGutterMarker(i, "gutter-opcodes", opcodeElement);
+
+        address = results.nextAddress;
+    }
+
+    const after = Date.now();
+    console.log("Assembly time: " + (after - before));
+}
+
 export function main() {
     const element = document.getElementById("editor") as HTMLElement;
     const config = {
@@ -16,35 +52,15 @@ export function main() {
     };
     const cm = CodeMirror(element, config);
 
+    cm.on("change", (instance, changeObj) => {
+        // It's important to call this in "change", and not in "changes" or
+        // after a timeout, because we want to be part of this operation.
+        // This way a large re-assemble takes 40ms instead of 300ms.
+        assembleAll(cm);
+    });
+
     setTimeout(() => {
-        let address = 0;
-        const constants = {};
-
-        const before = Date.now();
-        for (let i = 0; i < cm.lineCount(); i++) {
-            const line = cm.getLine(i);
-            const parser = new Parser(line, address, constants, true);
-            const results = parser.assemble();
-            address = results.nextAddress;
-        }
-        address = 0;
-        for (let i = 0; i < cm.lineCount(); i++) {
-            const line = cm.getLine(i);
-            const parser = new Parser(line, address, constants, false);
-            const results = parser.assemble();
-
-            const addressString = results.address.toString(16).toUpperCase().padStart(4, "0");
-            const addressElement = document.createTextNode(addressString) as any as HTMLElement;
-            cm.setGutterMarker(i, "gutter-address", addressElement);
-
-            const opcodeString = results.binary.map(opcode => opcode.toString(16).toUpperCase().padStart(2, "0")).join(" ");
-            const opcodeElement = document.createTextNode(opcodeString) as any as HTMLElement;
-            cm.setGutterMarker(i, "gutter-opcodes", opcodeElement);
-
-            address = results.nextAddress;
-        }
-        const after = Date.now();
-        console.log("Assembly time: " + (after - before));
+        assembleAll(cm);
     }, 100);
 }
 
