3a4f9401ec943b073408a4ea8c970cd2d29ca8af
Lawrence Kesteloot <lk@teamten.com>
2020-04-04 12:38:55 -0700

    Better sample code.

--- packages/z80-asm/src/ide/Ide.ts
+++ packages/z80-asm/src/ide/Ide.ts
@@ -62,7 +62,7 @@ export function main() {
         assembleAll(cm);
     });
 
-    fetch("samples/sio_basic.asm")
+    fetch("samples/basic.asm")
         .then(response => response.text())
         .then(text => cm.setValue(text));
 }
