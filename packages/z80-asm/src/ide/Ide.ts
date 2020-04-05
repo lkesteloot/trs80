5299244b3c00aa80da58ac35eb5d67711c07671b
Lawrence Kesteloot <lk@teamten.com>
2020-04-04 17:59:24 -0700

    Split long opcode lists over multiple lines.

--- packages/z80-asm/src/ide/Ide.ts
+++ packages/z80-asm/src/ide/Ide.ts
@@ -1,6 +1,15 @@
 
 import CodeMirror from "codemirror";
 import {Parser} from "../assembler/Parser";
+import {toHexByte, toHexWord} from "z80-base";
+
+// Max number of sub-lines per line. These are lines where we display the
+// opcodes for a single source line.
+const MAX_SUBLINES = 100;
+
+// Max number of opcode bytes we display per subline. Sync this with the CSS
+// that lays out the gutter.
+const BYTES_PER_SUBLINE = 3;
 
 function assembleAll(cm: CodeMirror.Editor) {
     const constants = {};
@@ -25,12 +34,29 @@ function assembleAll(cm: CodeMirror.Editor) {
 
         let addressElement: HTMLElement | null;
         if (results.binary.length > 0) {
-            const addressString = results.address.toString(16).toUpperCase().padStart(4, "0") +
-                " " + results.binary.map(opcode => opcode.toString(16).toUpperCase().padStart(2, "0")).join(" ");
-            const addressTextElement = document.createTextNode(addressString);
-            addressElement = document.createElement("span");
-            addressElement.appendChild(addressTextElement);
+            addressElement = document.createElement("div");
+
+            // Break opcodes over multiple lines if necessary.
+            let numLines = 0;
+            for (let offset = 0;
+                 offset < results.binary.length && numLines < MAX_SUBLINES;
+                 offset += BYTES_PER_SUBLINE, numLines++) {
+
+                const addressString = toHexWord(results.address + offset) +
+                    " " + results.binary.slice(offset, offset + BYTES_PER_SUBLINE).map(toHexByte).join(" ");
+                const addressTextElement = document.createTextNode(addressString);
+                if (offset > 0) {
+                    addressElement.appendChild(document.createElement("br"));
+                }
+                addressElement.appendChild(addressTextElement);
+            }
             addressElement.classList.add("gutter-style");
+
+            // For the line height using CSS.
+            cm.removeLineClass(i, "wrap", undefined);
+            if (numLines !== 1) {
+                cm.addLineClass(i, "wrap", "line-height-" + numLines);
+            }
         } else {
             addressElement = null;
         }
@@ -59,6 +85,16 @@ export function main() {
         gutters: ["CodeMirror-linenumbers", "gutter-assembled"],
     };
     const cm = CodeMirror(element, config);
+
+    // Create CSS classes for our line heights. We do this dynamically since
+    // we don't know the line height at compile time.
+    const cssRules = [];
+    for (let lineCount = 1; lineCount < MAX_SUBLINES; lineCount++) {
+        cssRules.push(".line-height-" + lineCount + " { height: " + lineCount*cm.defaultTextHeight() + "px; }");
+    }
+    const style = document.createElement("style");
+    style.appendChild(document.createTextNode(cssRules.join("\n")));
+    document.head.appendChild(style);
 
     cm.on("change", (instance, changeObj) => {
         // It's important to call this in "change", and not in "changes" or
