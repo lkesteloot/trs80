e057269f6890de9dfadee597b8f809b99a48b1d1
Lawrence Kesteloot <lk@teamten.com>
2020-04-04 18:05:34 -0700

    Fix large constants and negative jumps.

--- packages/z80-asm/src/assembler/Parser.ts
+++ packages/z80-asm/src/assembler/Parser.ts
@@ -291,11 +291,11 @@ export class Parser {
 
                                 case "nn":
                                 case "dd":
-                                    this.results.binary.push(value);
+                                    this.results.binary.push(lo(value));
                                     break;
 
                                 case "offset":
-                                    this.results.binary.push(value - this.results.address - this.results.binary.length - 1);
+                                    this.results.binary.push(lo(value - this.results.address - this.results.binary.length - 1));
                                     break;
 
                                 default:
