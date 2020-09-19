b69b85b754083139f5610c77b0a1cb7bf0f96890
Lawrence Kesteloot <lk@teamten.com>
2020-09-18 21:09:35 -0700

    Add control panel bling.

--- packages/trs80-emulator/src/ControlPanel.ts
+++ packages/trs80-emulator/src/ControlPanel.ts
@@ -1,15 +1,20 @@
 
+import {CSS_PREFIX} from "./Utils";
+
+const gCssPrefix = CSS_PREFIX + "-control-panel";
+const gScreenNodeCssClass = gCssPrefix + "-screen-node";
+const gPanelCssClass = gCssPrefix + "-panel";
+const gButtonCssClass = gCssPrefix + "-button";
+
 // https://thenounproject.com/search/?q=reset&i=3012384
 const RESET_ICON = `
-<svg xmlns:x="http://ns.adobe.com/Extensibility/1.0/" xmlns:i="http://ns.adobe.com/AdobeIllustrator/10.0/" xmlns:graph="http://ns.adobe.com/Graphs/1.0/" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="30" height="30" viewBox="0 0 100 100" xml:space="preserve">
     <switch>
-        <g fill="white" i:extraneous="self">
-            <g>
-                <path d="M5273.1,2400.1v-2c0-2.8-5-4-9.7-4s-9.7,1.3-9.7,4v2c0,1.8,0.7,3.6,2,4.9l5,4.9c0.3,0.3,0.4,0.6,0.4,1v6.4     c0,0.4,0.2,0.7,0.6,0.8l2.9,0.9c0.5,0.1,1-0.2,1-0.8v-7.2c0-0.4,0.2-0.7,0.4-1l5.1-5C5272.4,2403.7,5273.1,2401.9,5273.1,2400.1z      M5263.4,2400c-4.8,0-7.4-1.3-7.5-1.8v0c0.1-0.5,2.7-1.8,7.5-1.8c4.8,0,7.3,1.3,7.5,1.8C5270.7,2398.7,5268.2,2400,5263.4,2400z"/>
-                <path d="M5268.4,2410.3c-0.6,0-1,0.4-1,1c0,0.6,0.4,1,1,1h4.3c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H5268.4z"/>
-                <path d="M5272.7,2413.7h-4.3c-0.6,0-1,0.4-1,1c0,0.6,0.4,1,1,1h4.3c0.6,0,1-0.4,1-1C5273.7,2414.1,5273.3,2413.7,5272.7,2413.7z"/>
-                <path d="M5272.7,2417h-4.3c-0.6,0-1,0.4-1,1c0,0.6,0.4,1,1,1h4.3c0.6,0,1-0.4,1-1C5273.7,2417.5,5273.3,2417,5272.7,2417z"/>
-            </g>
+        <g fill="white">
+            <path d="M5273.1,2400.1v-2c0-2.8-5-4-9.7-4s-9.7,1.3-9.7,4v2c0,1.8,0.7,3.6,2,4.9l5,4.9c0.3,0.3,0.4,0.6,0.4,1v6.4     c0,0.4,0.2,0.7,0.6,0.8l2.9,0.9c0.5,0.1,1-0.2,1-0.8v-7.2c0-0.4,0.2-0.7,0.4-1l5.1-5C5272.4,2403.7,5273.1,2401.9,5273.1,2400.1z      M5263.4,2400c-4.8,0-7.4-1.3-7.5-1.8v0c0.1-0.5,2.7-1.8,7.5-1.8c4.8,0,7.3,1.3,7.5,1.8C5270.7,2398.7,5268.2,2400,5263.4,2400z"/>
+            <path d="M5268.4,2410.3c-0.6,0-1,0.4-1,1c0,0.6,0.4,1,1,1h4.3c0.6,0,1-0.4,1-1c0-0.6-0.4-1-1-1H5268.4z"/>
+            <path d="M5272.7,2413.7h-4.3c-0.6,0-1,0.4-1,1c0,0.6,0.4,1,1,1h4.3c0.6,0,1-0.4,1-1C5273.7,2414.1,5273.3,2413.7,5272.7,2413.7z"/>
+            <path d="M5272.7,2417h-4.3c-0.6,0-1,0.4-1,1c0,0.6,0.4,1,1,1h4.3c0.6,0,1-0.4,1-1C5273.7,2417.5,5273.3,2417,5272.7,2417z"/>
             <path d="M84.3,18C67.1,0.8,39.5,0.4,21.8,16.5l-4.1-4.1c-1.6-1.6-4-2.2-6.2-1.6c-2.2,0.7-3.9,2.5-4.3,4.7L2.6,36.9    c-0.4,2.1,0.2,4.2,1.7,5.7c1.5,1.5,3.6,2.1,5.7,1.7l21.4-4.5c1.2-0.3,2.3-0.9,3.1-1.7c0.7-0.7,1.3-1.6,1.6-2.6    c0.6-2.2,0-4.6-1.6-6.2l-3.9-3.9C43.5,14,63.1,14.5,75.4,26.8c12.8,12.8,12.8,33.6,0,46.4C62.6,86,41.8,86,29,73.2    c-4.1-4.1-7-9.2-8.5-14.8c-0.9-3.3-4.3-5.3-7.6-4.4c-3.3,0.9-5.3,4.3-4.4,7.6c2,7.7,6.1,14.8,11.8,20.4    c17.7,17.7,46.4,17.7,64.1,0C101.9,64.4,101.9,35.6,84.3,18z"/>
         </g>
     </switch>
@@ -18,7 +23,7 @@ const RESET_ICON = `
 
 // https://thenounproject.com/search/?q=camera&i=1841396
 const CAMERA_ICON = `
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+<svg xmlns="http://www.w3.org/2000/svg"  version="1.1" x="0px" y="0px" width="30" height="30" viewBox="0 0 100 100" xml:space="preserve">
     <g fill="white">
         <circle cx="50" cy="55.4" r="13.8"/>
         <path d="M80.6,25.4H67.1l-1.8-7.2c-0.5-2.1-2.5-3.6-4.7-3.6H39.3c-2.2,0-4.1,1.5-4.7,3.6l-1.8,7.2H19.4C11.5,25.4,5,31.9,5,39.8V71   c0,7.9,6.5,14.4,14.4,14.4h61.2C88.5,85.4,95,78.9,95,71V39.8C95,31.9,88.5,25.4,80.6,25.4z M50,76.4c-11.6,0-21-9.4-21-21   s9.4-21,21-21s21,9.4,21,21S61.6,76.4,50,76.4z M81.4,40.3c-2,0-3.6-1.6-3.6-3.6c0-2,1.6-3.6,3.6-3.6s3.6,1.6,3.6,3.6   C85,38.7,83.4,40.3,81.4,40.3z"/>
@@ -28,13 +33,50 @@ const CAMERA_ICON = `
 
 // https://thenounproject.com/search/?q=previous%20track&i=658409
 const PREVIOUS_TRACK_ICON = `
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 14 21.25" version="1.1" x="0px" y="0px">
+<svg xmlns="http://www.w3.org/2000/svg"  width="30" height="30" viewBox="-1 -2 16 21" version="1.1" x="0px" y="0px">
     <g fill="white" fill-rule="evenodd">
         <g transform="translate(-320.000000, -618.000000)">
             <path d="M330,628.032958 L330,634.00004 C330,634.545291 330.45191,635 331.009369,635 L332.990631,635 C333.556647,635 334,634.552303 334,634.00004 L334,618.99996 C334,618.454709 333.54809,618 332.990631,618 L331.009369,618 C330.443353,618 330,618.447697 330,618.99996 L330,624.967057 C329.894605,624.850473 329.775773,624.739153 329.643504,624.634441 L322.356496,618.865559 C321.054403,617.834736 320,618.3432 320,620.000122 L320,632.999878 C320,634.663957 321.055039,635.164761 322.356496,634.134441 L329.643504,628.365559 C329.775779,628.260841 329.894611,628.149527 330,628.032958 Z" transform="translate(327.000000, 626.500000) scale(-1, 1) translate(-327.000000, -626.500000) "/>
         </g>
     </g>
 </svg>
+`;
+
+const GLOBAL_CSS = "." + gPanelCssClass + ` {
+    background-color: rgba(0, 0, 0, 0.2);
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    border-radius: 5px;
+    opacity: 0;
+    transition: opacity .20s ease-in-out;
+}
+
+.` + gScreenNodeCssClass + ` {
+    /* Force the screen node to relative positioning. Hope that doesn't screw anything up. */
+    position: relative;
+}
+
+.` + gScreenNodeCssClass + `:hover .` + gPanelCssClass + ` {
+    opacity: 1;
+}
+
+.` + gButtonCssClass + ` {
+    display: block;
+    margin: 15px;
+    cursor: pointer;
+    opacity: 0.5;
+    transition: opacity .05s ease-in-out;
+    transition: transform 0.05s ease-in-out;
+}
+
+.` + gButtonCssClass + `:hover {
+    opacity: 1;
+}
+
+.` + gButtonCssClass + `:active {
+    transform: scale(1.15);
+}
 `;
 
 /**
@@ -47,27 +89,14 @@ export class ControlPanel {
      * @param screenNode the node from the Trs80Screen object's getNode() method.
      */
     constructor(screenNode: HTMLElement) {
-        // Force the screen node to relative positioning. Hope that doesn't screw anything up.
-        screenNode.style.position = "relative";
+        // Make global CSS if necessary.
+        ControlPanel.configureStyle();
+
+        screenNode.classList.add(gScreenNodeCssClass);
 
         this.panelNode = document.createElement("div");
-        this.panelNode.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
-        this.panelNode.style.position = "absolute";
-        this.panelNode.style.right = "10px";
-        this.panelNode.style.top = "10px";
-        this.panelNode.style.width = "50px";
-        // this.panelNode.style.height = "150px";
-        this.panelNode.style.borderRadius = "5px";
-        this.panelNode.style.padding = "4px";
-
+        this.panelNode.classList.add(gPanelCssClass);
         screenNode.appendChild(this.panelNode);
-
-        screenNode.addEventListener("mouseenter", () => {
-            this.panelNode.style.display = "block";
-        });
-        screenNode.addEventListener("mouseleave", () => {
-            this.panelNode.style.display = "none";
-        });
     }
 
     /**
@@ -75,9 +104,8 @@ export class ControlPanel {
      */
     public addResetButton(callback: () => void) {
         let icon = document.createElement("img");
+        icon.classList.add(gButtonCssClass);
         icon.src = "data:image/svg+xml;base64," + btoa(RESET_ICON);
-        icon.style.transform = "scale(0.5)";
-        icon.style.cursor = "pointer";
         icon.title = "Reset the computer";
         icon.addEventListener("click", callback);
         this.panelNode.appendChild(icon);
@@ -88,9 +116,8 @@ export class ControlPanel {
      */
     public addScreenshotButton(callback: () => void) {
         let icon = document.createElement("img");
+        icon.classList.add(gButtonCssClass);
         icon.src = "data:image/svg+xml;base64," + btoa(CAMERA_ICON);
-        icon.style.transform = "scale(0.5)";
-        icon.style.cursor = "pointer";
         icon.title = "Take a screenshot";
         icon.addEventListener("click", callback);
         this.panelNode.appendChild(icon);
@@ -101,11 +128,26 @@ export class ControlPanel {
      */
     public addTapeRewindButton(callback: () => void) {
         let icon = document.createElement("img");
+        icon.classList.add(gButtonCssClass);
         icon.src = "data:image/svg+xml;base64," + btoa(PREVIOUS_TRACK_ICON);
-        icon.style.transform = "scale(0.32)";
-        icon.style.cursor = "pointer";
         icon.title = "Rewind the cassette";
         icon.addEventListener("click", callback);
         this.panelNode.appendChild(icon);
+    }
+
+    /**
+     * Make a global stylesheet for all TRS-80 emulators on this page.
+     */
+    private static configureStyle(): void {
+        const styleId = gCssPrefix;
+        if (document.getElementById(styleId) !== null) {
+            // Already created.
+            return;
+        }
+
+        const node = document.createElement("style");
+        node.id = styleId;
+        node.innerHTML = GLOBAL_CSS;
+        document.head.appendChild(node);
     }
 }
