562f1879c744c06d312599102cd519f0df20b1e1
Lawrence Kesteloot <lk@teamten.com>
2020-10-04 18:13:33 -0700

    Can read BAS files.

--- packages/trs80-cassette-reader-js/src/Uploader.ts
+++ packages/trs80-cassette-reader-js/src/Uploader.ts
@@ -90,10 +90,39 @@ export class Uploader {
     private handleArrayBuffer(pathname: string, arrayBuffer: ArrayBuffer) {
         const rate = 44100;
 
-        const audioFile = pathname.toLowerCase().endsWith(".cas")
-            ? new AudioFile(rate, encodeLowSpeed(new Uint8Array(arrayBuffer), rate))
-            : readWavFile(arrayBuffer);
+        let audioFile;
+        if (pathname.toLowerCase().endsWith(".cas")) {
+            audioFile = new AudioFile(rate, encodeLowSpeed(new Uint8Array(arrayBuffer), rate));
+        } else if (pathname.toLowerCase().endsWith(".bas")) {
+            // Add tape header.
+            const buffers = [
+                new Uint8Array(256),
+                new Uint8Array([0xA5, 0xD3, 0xD3, 0xD3]),
+                new Uint8Array(arrayBuffer),
+            ];
+            const allBytes = this.concatByteArrays(buffers);
+
+            audioFile = new AudioFile(rate, encodeLowSpeed(allBytes, rate));
+        } else {
+            audioFile = readWavFile(arrayBuffer);
+        }
         this.handleAudioBuffer(pathname, audioFile);
+    }
+
+    /**
+     * Concatenate a list of byte arrays into one.
+     */
+    private concatByteArrays(samplesList: Uint8Array[]): Uint8Array {
+        const length = samplesList.reduce((sum, samples) => sum + samples.length, 0);
+        const allBytes = new Uint8Array(length);
+
+        let offset = 0;
+        for (const samples of samplesList) {
+            allBytes.set(samples, offset);
+            offset += samples.length;
+        }
+
+        return allBytes;
     }
 
     private dropHandler(ev: DragEvent) {
