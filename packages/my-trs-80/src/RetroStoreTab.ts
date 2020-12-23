b5d900e3a9c945615e9bf052c08c6155f418398e
Lawrence Kesteloot <lk@teamten.com>
2020-12-22 20:13:21 -0800

    Load RetroStore apps on demand.

--- packages/my-trs-80/src/RetroStoreTab.ts
+++ packages/my-trs-80/src/RetroStoreTab.ts
@@ -9,16 +9,28 @@ import {FileBuilder} from "./File";
 
 const RETRO_STORE_API_URL = "https://retrostore.org/api/";
 
+const APP_FETCH_COUNT = 10;
+
+/**
+ * Stores info about a RetroStore app and its media.
+ */
+class RetroStoreApp {
+    public readonly app: RetroStoreProto.App;
+    public element: HTMLElement | undefined = undefined;
+
+    constructor(app: RetroStoreProto.App) {
+        this.app = app;
+    }
+}
+
 /**
  * Fetch all apps from RetroStore. If an error occurs, returns an empty list.
  */
-function fetchApps(): Promise<RetroStoreProto.App[]> {
-    const start = 0;
-    const num = 10;
+function fetchApps(start: number, count: number): Promise<RetroStoreProto.App[]> {
     const query = "";
     const apiRequest = {
         start: start,
-        num: num,
+        num: count,
         query: query,
         trs80: {
             mediaTypes: [],
@@ -81,13 +93,13 @@ function fetchMediaImages(appId: string): Promise<RetroStoreProto.MediaImage[]> 
             } else {
                 // TODO.
                 console.error("Can't get media images for " + appId + ": " + mediaImages.message);
-                return Promise.resolve([]);
+                return Promise.reject();
             }
         })
         .catch(error => {
             // TODO
             console.error(error);
-            return Promise.resolve([]);
+            return Promise.reject();
         });
 }
 
@@ -97,6 +109,10 @@ function fetchMediaImages(appId: string): Promise<RetroStoreProto.MediaImage[]> 
 export class RetroStoreTab {
     private readonly context: Context;
     private readonly appsDiv: HTMLElement;
+    private readonly moreDiv: HTMLElement;
+    private readonly apps: RetroStoreApp[] = [];
+    private complete = false;
+    private fetching = false;
 
     constructor(pageTabs: PageTabs, context: Context) {
         this.context = context;
@@ -106,36 +122,77 @@ export class RetroStoreTab {
 
         this.appsDiv = document.createElement("div");
         this.appsDiv.classList.add("retro-store-apps");
+        this.appsDiv.addEventListener("scroll", () => this.fetchNextBatchIfNecessary());
         tab.element.append(this.appsDiv);
 
-        fetchApps()
-            .then(apps => {
-                this.populateApps(apps);
-            })
-            .catch(error => {
-                // TODO. We already catch errors in fetchApps.
-                console.error(error);
-            });
+        this.moreDiv = document.createElement("div");
+        this.moreDiv.classList.add("retro-store-more");
+        this.moreDiv.append(makeIcon("cached"));
+
+        // When showing the tab, wait for laying and maybe fetch more.
+        tab.onShow.subscribe(() => setTimeout(() => this.fetchNextBatchIfNecessary(), 0));
+        this.populateApps();
     }
 
     /**
-     * Populate the UI with a list of apps.
+     * If the "More" section is visible, fetch more apps.
      */
-    private populateApps(apps: RetroStoreProto.App[]): void {
-        clearElement(this.appsDiv);
-
-        for (const app of apps) {
-            this.addApp(app);
+    private fetchNextBatchIfNecessary(): void {
+        const moreVisible = this.moreDiv.getBoundingClientRect().top < this.appsDiv.getBoundingClientRect().bottom;
+        if (moreVisible && !this.complete && !this.fetching) {
+            this.fetchNextBatch();
         }
     }
 
     /**
-     * Add a specific app to the UI.
+     * Get the next batch of apps if necessary.
      */
-    private addApp(app: RetroStoreProto.App): void {
+    private fetchNextBatch(): void {
+        if (!this.complete) {
+            this.fetching = true;
+            fetchApps(this.apps.length, APP_FETCH_COUNT)
+                .then(apps => {
+                    this.fetching = false;
+                    if (apps.length !== APP_FETCH_COUNT) {
+                        // Got all apps.
+                        this.complete = true;
+                    }
+                    this.apps.push(...apps.map(a => new RetroStoreApp(a)));
+                    this.populateApps();
+                })
+                .catch(error => {
+                    // TODO.
+                    console.error(error);
+                    this.fetching = false;
+                    this.complete = true;
+                });
+        }
+    }
+
+    /**
+     * Populate the UI with the apps we have.
+     */
+    private populateApps(): void {
+        clearElement(this.appsDiv);
+
+        for (const app of this.apps) {
+            if (app.element === undefined) {
+                app.element = this.createAppTile(app.app);
+            }
+            this.appsDiv.append(app.element);
+        }
+
+        if (!this.complete) {
+            this.appsDiv.append(this.moreDiv);
+        }
+    }
+
+    /**
+     * Create a tile for an app.
+     */
+    private createAppTile(app: RetroStoreProto.App): HTMLElement {
         const appDiv = document.createElement("div");
         appDiv.classList.add("retro-store-app");
-        this.appsDiv.append(appDiv);
 
         const screenshotDiv = document.createElement("img");
         screenshotDiv.classList.add("screenshot");
@@ -169,6 +226,7 @@ export class RetroStoreTab {
             versionDiv.innerText = "Version " + app.version;
             appDiv.append(versionDiv);
         }
+
         if (app.description !== undefined && app.description !== "") {
             const descriptionDiv = document.createElement("div");
             descriptionDiv.classList.add("description");
@@ -245,5 +303,7 @@ export class RetroStoreTab {
                     console.error(error);
                 });
         }
+
+        return appDiv;
     }
 }
