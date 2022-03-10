99cee66fe9cf8db97ce09ecacf4f6dd537b108b1
Lawrence Kesteloot <lk@teamten.com>
2022-03-09 21:24:21 -0800

    trs80-ide: Add CodeMirror 6.

--- packages/trs80-ide/src/index.ts
+++ packages/trs80-ide/src/index.ts
@@ -1,11 +1,14 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-import confetti from 'canvas-confetti';
+import {EditorState} from "@codemirror/state"
+import {EditorView, keymap} from "@codemirror/view"
+import {defaultKeymap} from "@codemirror/commands"
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+let startState = EditorState.create({
+  doc: "Hello World",
+  extensions: [keymap.of(defaultKeymap)]
+});
+
+let view = new EditorView({
+  state: startState,
+  parent: document.getElementById("editor") as HTMLDivElement
+});
