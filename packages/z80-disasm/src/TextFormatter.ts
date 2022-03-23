c509bba1e50099a24d25f14f9e563da806b80017
Lawrence Kesteloot <lk@teamten.com>
2022-03-22 22:29:40 -0700

    z80-disasm: Can generate plain asm code.

--- packages/z80-disasm/src/TextFormatter.ts
+++ packages/z80-disasm/src/TextFormatter.ts
@@ -1,28 +1,47 @@
 import {Instruction} from "./Instruction.js";
 import {toHexByte, toHexWord} from "z80-base";
 
+const INSTRUCTION_INDENT = "        ";
+const LISTING_INDENT = "                 ";
+
 /**
  * Converts an array of instructions into an array of text lines, suitable for displaying
  * in a shell console.
+ *
+ * @param instructions The array of instructions to convert.
+ * @param makeListing Make a listing (vs. disassembly).
+ * @returns The array of text lines.
  */
-export function instructionsToText(instructions: Instruction[]): string[] {
+export function instructionsToText(instructions: Instruction[], makeListing: boolean): string[] {
     const lines: string[] = [];
 
-    for (const instruction of instructions) {
-        if (instruction.label !== undefined) {
-            lines.push("                 " + instruction.label + ":");
-        }
+    let expectedAddress = undefined;
 
+    for (const instruction of instructions) {
         let address = instruction.address;
         const bytes = instruction.bin;
 
-        while (bytes.length > 0) {
-            const subbytes = bytes.slice(0, Math.min(3, bytes.length));
-            lines.push(toHexWord(address) + " " +
-                subbytes.map(toHexByte).join(" ").padEnd(12) +
-                (address === instruction.address ? "        " + instruction.toText() : ""));
-            address += subbytes.length;
-            bytes.splice(0, subbytes.length);
+        if (address !== expectedAddress) {
+            lines.push((makeListing ? LISTING_INDENT : "") + INSTRUCTION_INDENT + ".org 0x" + toHexWord(address));
+            expectedAddress = address;
+        }
+        expectedAddress += bytes.length;
+
+        if (instruction.label !== undefined) {
+            lines.push((makeListing ? LISTING_INDENT : "") + instruction.label + ":");
+        }
+
+        if (makeListing) {
+            while (bytes.length > 0) {
+                const subbytes = bytes.slice(0, Math.min(3, bytes.length));
+                lines.push(toHexWord(address) + " " +
+                    subbytes.map(toHexByte).join(" ").padEnd(12) +
+                    (address === instruction.address ? INSTRUCTION_INDENT + instruction.toText() : ""));
+                address += subbytes.length;
+                bytes.splice(0, subbytes.length);
+            }
+        } else {
+            lines.push(INSTRUCTION_INDENT + instruction.toText());
         }
     }
 
