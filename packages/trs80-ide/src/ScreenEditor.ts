a78b27dcf03c7187d839c62be11ce421262dba77
Lawrence Kesteloot <lk@teamten.com>
2022-05-21 17:06:14 -0700

    trs80-ide: screenshot handle too much or too little data.

--- packages/trs80-ide/src/ScreenEditor.ts
+++ packages/trs80-ide/src/ScreenEditor.ts
@@ -1,9 +1,8 @@
-
 import {EditorView} from "@codemirror/view"
-import {TRS80_SCREEN_BEGIN, TRS80_SCREEN_SIZE } from 'trs80-base';
-import { Trs80 } from 'trs80-emulator';
-import { CanvasScreen, ScreenMouseEvent } from 'trs80-emulator-web';
-import { toHexByte } from 'z80-base';
+import {TRS80_SCREEN_BEGIN, TRS80_SCREEN_SIZE} from 'trs80-base';
+import {Trs80} from 'trs80-emulator';
+import {CanvasScreen, ScreenMouseEvent} from 'trs80-emulator-web';
+import {toHexByte} from 'z80-base';
 import {AssemblyResults} from "./AssemblyResults.js";
 
 /**
@@ -14,8 +13,10 @@ export class ScreenEditor {
     private readonly view: EditorView;
     private readonly screen: CanvasScreen;
     private readonly raster = new Uint8Array(TRS80_SCREEN_SIZE);
+    private readonly extraBytes: number[];
     private readonly begin: number;
     private end: number;
+    private byteCount: number;
     private mouseDown = false;
 
     constructor(view: EditorView, pos: number, assemblyResults: AssemblyResults,
@@ -27,16 +28,23 @@ export class ScreenEditor {
 
         trs80.stop();
 
+        // Fill with blanks.
+        this.raster.fill(0x80);
+
         const s = assemblyResults.screenshotSections[screenshotIndex];
+        this.byteCount = s.byteCount;
+        this.extraBytes = [];
         if (s.firstDataLineNumber !== undefined && s.lastDataLineNumber !== undefined) {
             let i = 0;
             for (let lineNumber = s.firstDataLineNumber; lineNumber <= s.lastDataLineNumber; lineNumber++) {
                 const line = assemblyResults.sourceFile.assembledLines[lineNumber - 1];
-                for (const c of line.binary) {
-                    if (i === this.raster.length) {
-                        break;
-                    }
-                    this.raster[i++] = c;
+                const length = Math.min(line.binary.length, this.raster.length - i);
+                for (let j = 0; j < length; j++) {
+                    this.raster[i++] = line.binary[j];
+                }
+                if (length < line.binary.length) {
+                    this.extraBytes = line.binary.slice(length);
+                    break;
                 }
             }
             this.begin = view.state.doc.line(s.firstDataLineNumber).from;
@@ -55,12 +63,22 @@ export class ScreenEditor {
      */
     private rasterToCode() {
         const lines = [];
-        for (let i = 0; i < this.raster.length; i += 8) {
-            const bytes = Array.from(this.raster.subarray(i, i + 8));
+        const raster = Array.from(this.raster.subarray(0, this.byteCount));
+        for (let i = 0; i < raster.length; i += 8) {
+            const bytes = raster.slice(i, i + 8);
             const bytesStrings = bytes.map(b => "0x" + toHexByte(b));
             // TODO guess the indent.
             // TODO guess ASCII and use .text.
-            lines.push(`        .byte ${bytesStrings.join(",")}`);
+            let text = `        .byte ${bytesStrings.join(",")}`;
+            if (i % 64 == 0) {
+                text += ` ; Line ${i / 64}`;
+            }
+            lines.push(text);
+        }
+        if (this.extraBytes.length > 0) {
+            const bytesStrings = this.extraBytes.map(b => "0x" + toHexByte(b));
+            let text = `        .byte ${bytesStrings.join(",")} ; Extra data`;
+            lines.push(text);
         }
         const code = lines.join("\n");
 
@@ -104,6 +122,7 @@ export class ScreenEditor {
             ch |= position.mask | 0x80;
             this.raster[position.offset] = ch;
             this.screen.writeChar(position.address, ch);
+            this.byteCount = Math.max(this.byteCount, position.offset + 1);
         }
     }
 }
