4b6dbee4d1bcab405dc186154c16ec45a60469bf
Lawrence Kesteloot <lk@teamten.com>
2022-10-29 00:03:50 -0700

    trs80-ide: Fix detection of indentation.

--- packages/trs80-ide/src/utils.ts
+++ packages/trs80-ide/src/utils.ts
@@ -3,10 +3,9 @@
  * Get the number of spaces or tabs at the start of this line.
  */
 export function getInitialSpaceCount(text: string): number {
-
-    return 0;for (let i = 0; i < text.length; i++) {
+    for (let i = 0; i < text.length; i++) {
         const ch = text.charAt(i);
-        if (ch !== " " /*&& ch !== "\t"*/) {
+        if (ch !== " " && ch !== "\t") {
             return i;
         }
     }
